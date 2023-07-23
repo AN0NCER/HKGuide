18ea73bbeff98c9c2a7d986ded237e7ec09b63bf
Anoncer <anoncer@protonmail.com>
2023-07-23 23:58:22 +0200

    Configurate

---
+++ HKGuide/HKGuide.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Modding;
+using UnityEngine;
+
+namespace HKGuide
+{
+    public class HKGuide : Mod
+    {
+        new public string GetName() => "HKGuide";
+        public override string GetVersion() => "v1";
+
+        public override void Initialize()
+        {
+            ModHooks.HeroUpdateHook += OnHeroUpdate;
+        }
+
+        public void OnHeroUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+                Log("Press Key O");
+            }
+        }
+    }
+}
